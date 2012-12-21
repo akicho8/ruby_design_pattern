a823bffe2815539e59e2b4f9d51359c9ac58340c
akicho8 <akicho8@gmail.com>
2012-12-22 00:36:32 +0900

    facade追加

---
+++ facade.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Facade - シンプルな窓口
+
+# browser = Browser.open(:type => :firefox)
+# browser.command(:open, "http://www.google.co.jp")
+# binary = browser.command(:screenshot)
+# img = RMagick.convert(binary, :type => :jpg)
+# ...
+
+# Image.capture("http://www.google.co.jp")
