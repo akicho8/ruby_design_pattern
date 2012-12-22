9f01ea63a0760ffdc1cbf1148c884c26713aec9e
akicho8 <akicho8@gmail.com>
2012-12-22 18:50:01 +0900

    コードブロックを使ったストラテジー追加

--- strategy.rb
+++ strategy.rb
@@ -21,5 +21,19 @@ class Player
   end
 end
 
-Player.new(Random.new).run  # => [1, 5, 4, 1, 0, 0, 6]
+Player.new(Random.new).run  # => [3, 1, 4, 1, 2, 3, 6]
 Player.new(RedOnly.new).run # => [6, 6, 6, 6, 6, 6, 6]
+
+# RandomやRedOnlyを作るのが面倒→クラスの爆発
+# これを解消するには→コードブロックを使ったストラテジー
+class Player2
+  def initialize(&dice)
+    @dice = dice
+  end
+  def run
+    7.times.collect{@dice.call}
+  end
+end
+
+Player2.new{rand(7)}.run  # => [2, 5, 5, 6, 6, 2, 0]
+Player2.new{6}.run        # => [6, 6, 6, 6, 6, 6, 6]
