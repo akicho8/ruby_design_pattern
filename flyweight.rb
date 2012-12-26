1b8b2ce8572d5263955d419f3d722429b383bf68
akicho8 <akicho8@gmail.com>
2012-12-27 00:00:41 +0900

    flyweight追加

---
+++ flyweight.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Flyweight - 同じ物を共有して無駄を無くす
+
+module Wave
+  def self.load(file)
+    p "load #{file}"
+    file
+  end
+end
+
+class Sound
+  def self.get(name)
+    @cache ||= {}
+    @cache[name] ||= Wave.load("#{name}.wav")
+  end
+end
+
+Sound.get("blue")               # => "blue.wav"
+Sound.get("cyan")               # => "cyan.wav"
+Sound.get("blue")               # => "blue.wav"
+# >> "load blue.wav"
+# >> "load cyan.wav"
