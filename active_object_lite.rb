5c4a2db61fcc7fac2300c357c7bdf7e6adab4023
akicho8 <akicho8@gmail.com>
2013-01-12 19:47:49 +0900

    Active Object 追加

---
+++ active_object_lite.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Active Object - 非同期メッセージを受け取る、能動的なオブジェクト
+
+require "thread"
+
+class C
+  def process
+    1 + 2
+  end
+end
+
+obj = C.new
+obj.process                        # => 3
+
+# ↓
+
+class C
+  attr_accessor :queue
+
+  def initialize
+    @queue = Queue.new
+    Thread.start do
+      loop { @queue.pop.call }
+    end
+  end
+
+  def process
+    @queue << proc{p 1 + 2}
+  end
+end
+
+obj = C.new
+obj.process
+nil until obj.queue.empty?
+
+# >> 3
