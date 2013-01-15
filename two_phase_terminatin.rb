b49fe26f0bf89ba5970e20caa25adbb94bdfcd9b
akicho8 <akicho8@gmail.com>
2013-01-16 01:41:03 +0900

    Two Phase Terminatin 書き直し

--- two_phase_terminatin.rb
+++ two_phase_terminatin.rb
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
-# Two-Phase Terminatin - あとかだづけしてから、おやすみなさい
+# Two-Phase Terminatin
 
-Thread.start do
-  begin
-  rescue
+t = Thread.start do
+  2.times do |i|
+    if Thread.current["interrupt"]
+      break
+    end
+    p "処理中: #{i}"
+    sleep(0.2)
   end
-end.join
+  p "終了処理"
+end
+sleep(0.1)
+t["interrupt"] = true
+t.join
+# >> "処理中: 0"
+# >> "終了処理"
