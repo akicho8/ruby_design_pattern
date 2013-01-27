94aac75acf3fd72fc568fd638ab3f4df46403b5b
akicho8 <akicho8@gmail.com>
2013-01-27 23:05:35 +0900

    Adapter書き直し(今までFactory Methodだと思っていたのはAdapterだった)

--- adapter.rb
+++ adapter.rb
@@ -1,14 +1,49 @@
 # -*- coding: utf-8 -*-
 
-# ダメなクラスを
-class Color
-  def red
-    "#00f"
+class C
+  def f1
+    "x"
   end
 end
 
-# ラップして再利用
-class Palette < Color
-  alias blue red
+# 継承版
+
+class C2 < C
+  def f2
+    f1 * 2
+  end
 end
-Palette.new.blue # => "#00f"
+
+# 委譲版
+
+class C3
+  def initialize
+    @c = C.new
+  end
+
+  def f1
+    @c.f1
+  end
+
+  def f2
+    f1 * 2
+  end
+end
+
+# 委譲版 (その2)
+
+require "delegate"
+
+class C4 < SimpleDelegator
+  def initialize
+    super(C.new)
+  end
+
+  def f2
+    f1 * 2
+  end
+end
+
+[C2.new.f1, C2.new.f2]      # => ["x", "xx"]
+[C3.new.f1, C3.new.f2]      # => ["x", "xx"]
+[C4.new.f1, C4.new.f2]      # => ["x", "xx"]
