8154151053f1df958d0cc8858405c6e5daed1cd0
akicho8 <akicho8@gmail.com>
2013-01-29 22:40:10 +0900

    Immutableの例を改良

--- immutable.rb
+++ immutable.rb
@@ -1,43 +1,14 @@
 # -*- coding: utf-8 -*-
-# Immutable - 壊したくとも壊せない
-# セッターが存在しないクラスを Immutable と言う
+# Immutable - メンバー変数が変更されないからどこからアクセスしても安全という意味
 
-$DEBUG = true
-
-class User
-  attr_reader :name, :password
-  def initialize(name, password)
-    @name = name
-    @password = password
-  end
-
-  def to_s
-    "[name=#{@name} password=#{@password}]"
-  end
-
-  def check
-    @name[0] == @password[0]     # changeを有効にすると個々で nil[0] を実行してしまいエラーになる
-  end
-
-  def change
-    a, b = @name, @password
-    if true
-      # @name, @password = nil, nil  # ここを有効にすると Immutable では無くなり check でエラーが出る
-      sleep(0.1)
-    end
-    @name, @password = a, b
+class C
+  attr_reader :v
+  def initialize(v)
+    @v = v
+    @v.freeze
   end
 end
 
-# User が immutable であれば true を返す。
-ary = []
-user = User.new("alice", "a")
-ary = 2.times.collect do
-  Thread.start do
-    2.times do
-      user.change             # 危険
-      user.check
-    end
-  end
-end
-ary.each(&:join)
+a = C.new("x")
+a.v.replace("y") rescue $! # => #<RuntimeError: can't modify frozen String>
+a.v += "y" rescue $!       # => #<NoMethodError: undefined method `v=' for #<C:0x007fb7d2885338 @v="x">>
