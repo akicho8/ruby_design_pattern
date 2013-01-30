6a034df3d1b23b3d9150c7bcb2b2f74d84a3a6c4
akicho8 <akicho8@gmail.com>
2013-01-30 23:03:38 +0900

    Single Threaded Execution 追加

---
+++ single_threaded_execution.rb
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Single Threaded Execution - この橋を渡れるのはたった１人
+
+require "thread"
+
+# mutex がない場合
+
+a = 0
+b = 0
+2.times.collect do
+  Thread.start do
+    2.times do
+      a += 1
+      Thread.pass
+      b += 1
+      p [a, b, (a == b)]
+    end
+  end
+end.each(&:join)
+
+# ある場合
+
+mutex = Mutex.new
+a = 0
+b = 0
+2.times.collect do
+  Thread.start do
+    2.times do
+      mutex.synchronize do
+        a += 1
+        Thread.pass
+        b += 1
+        p [a, b, (a == b)]
+      end
+    end
+  end
+end.each(&:join)
+# >> [2, 1, false]
+# >> [2, 2, true]
+# >> [4, 3, false]
+# >> [4, 4, true]
+# >> [1, 1, true]
+# >> [2, 2, true]
+# >> [3, 3, true]
+# >> [4, 4, true]
