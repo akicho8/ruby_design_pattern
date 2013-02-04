dbad4b0050f2a4b125975bc19713bb22d2fbc17d
akicho8 <akicho8@gmail.com>
2013-02-04 20:59:49 +0900

    Factory Method 書き直し

--- adapter.rb
+++ adapter.rb
@@ -47,3 +47,81 @@ end
 [C2.new.f1, C2.new.f2]      # => ["x", "xx"]
 [C3.new.f1, C3.new.f2]      # => ["x", "xx"]
 [C4.new.f1, C4.new.f2]      # => ["x", "xx"]
+
+# --------------------------------------------------------------------------------
+
+class User
+  def self.search(query)
+    ["name like ?", "%#{query}%"]
+  end
+end
+
+User.search("alice")                     # => ["name like ?", "%alice%"]
+
+class User
+  def self.search(*args)
+    UserSearch.new(self, *args).run
+  end
+end
+
+class UserSearch
+  def initialize(model, query)
+    @model = model
+    @query = query
+  end
+  def run
+    ["name like ?", "%#{@query}%"]
+  end
+end
+
+User.search("alice")   # => ["name like ?", "%alice%"]
+
+class Player
+  attr_accessor :color
+  def initialize
+    @color = :blue
+  end
+end
+
+# ここでプレイヤーの色は青なので #00F が欲しいとする
+
+# 動けばいい的なダメな方法
+
+class Player
+  def rgb
+    if @color == :blue
+      "#00F"
+    end
+  end
+end
+
+# 改善
+
+class ColorInfo
+  attr_accessor :color
+  def initialize(color)
+    @color = color
+  end
+  def rgb
+    "#00F"
+  end
+end
+
+class Player
+  def color_info
+    ColorInfo.new(@color)
+  end
+end
+
+Player.new.rgb                       # => "#00F"
+Player.new.color_info.rgb            # => "#00F"
+
+# 綺麗な形で拡張できる
+
+class ColorInfo
+  def human_name
+    "青"
+  end
+end
+
+Player.new.color_info.human_name     # => "青"
--- factory_method.rb
+++ factory_method.rb
@@ -1,77 +1,19 @@
 # -*- coding: utf-8 -*-
 
-class User
-  def self.search(query)
-    ["name like ?", "%#{query}%"]
+class X
+end
+
+class F
+  def create
+    X.new
   end
 end
 
-User.search("alice")                     # => ["name like ?", "%alice%"]
-
-class User
-  def self.search(*args)
-    UserSearch.new(self, *args).run
+class C
+  attr_accessor :v
+  def initialize(f)
+    @v = f.create
   end
 end
 
-class UserSearch
-  def initialize(model, query)
-    @model = model
-    @query = query
-  end
-  def run
-    ["name like ?", "%#{@query}%"]
-  end
-end
-
-User.search("alice")   # => ["name like ?", "%alice%"]
-
-class Player
-  attr_accessor :color
-  def initialize
-    @color = :blue
-  end
-end
-
-# ここでプレイヤーの色は青なので #00F が欲しいとする
-
-# 動けばいい的なダメな方法
-
-class Player
-  def rgb
-    if @color == :blue
-      "#00F"
-    end
-  end
-end
-
-# 改善
-
-class ColorInfo
-  attr_accessor :color
-  def initialize(color)
-    @color = color
-  end
-  def rgb
-    "#00F"
-  end
-end
-
-class Player
-  def color_info
-    ColorInfo.new(@color)
-  end
-end
-
-Player.new.rgb                       # => "#00F"
-Player.new.color_info.rgb            # => "#00F"
-
-# 綺麗な形で拡張できる
-
-class ColorInfo
-  def human_name
-    "青"
-  end
-end
-
-Player.new.color_info.human_name     # => "青"
+C.new(F.new).v                  # => #<X:0x007fc8690862e8>
